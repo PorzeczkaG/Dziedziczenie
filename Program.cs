0c1d0f663c245988a1ad380742c0bc22a5901741
PorzeczkaG <64199094+PorzeczkaG@users.noreply.github.com>
2020-04-28 19:40:44 +0200

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Dziedziczenie{
+    class CNazwisko
+    {
+        public void nazwisko()
+        {
+            Console.WriteLine("Nazwisko:    Kowalski");
+        }
+
+    }
+
+    class CImie : CNazwisko
+    {
+        public void imie()
+        {
+            Console.WriteLine("Imie:    Jan");
+
+        }
+    }
+    class osoba : CImie
+    {
+
+    }
+
+class Program
+    {
+        static void Main(string[] args)
+        {
+            osoba p1 = new osoba();
+            p1.imie();
+            p1.nazwisko();
+
+
+            Console.ReadKey();
+        }
+    }
+
+    }
+
